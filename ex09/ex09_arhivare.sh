36b27f80c07d6f0a2824d7c027eb8363c7832f75
Decebal95 <iordache.decebal04@gmail.com>
2026-05-18 20:30:36 +0300

    ex09:arhivare

---
+++ ex09/ex09_arhivare.sh
@@ -0,0 +1,11 @@
+echo "9; arhivare"
+echo "1"
+mkdir -p /tmp/ex09/projekt/src
+mkdir -p /tmp/ex09/projekt/docs
+mkdir -p /tmp/ex09/projekt/build
+mkdir -p /tmp/ex09/projekt/scratch
+
+
+touch -r tmpo/ex09/projekt/main.c util.c util.h
+touch -r tmpo/ex09/projekt/readme.md manual.md
+touch -r tmpo/ex09/projekt/scratch/tmp/bin
